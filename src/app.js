885cc3bcf44645a0811fef7613696cf9c7ef9800
Beza12 <117820916+Beza12@users.noreply.github.com>
2024-02-19 13:20:34 +0300

    Add weather information

--- src/app.js
+++ src/app.js
@@ -3,8 +3,34 @@ function currentWeather(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
 
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  timeElement.innerHTML = currentDate(date);
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
+
+function currentDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday ",
+    "Tueday",
+    "Wednesday",
+    "Thuday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
