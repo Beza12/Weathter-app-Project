f2b101eb0bf2c115f3311472e3e34e3efd9c084f
Beza12 <117820916+Beza12@users.noreply.github.com>
2024-02-19 10:36:00 +0300

    Adjust the search engine

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+function currentWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "e1o3b3dd533bfetc4a74207b639a450e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metrics`;
+  axios.get(apiUrl).then(currentWeather);
+}
+
 function citySearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", citySearch);
+
+searchCity("Addis Ababa");
